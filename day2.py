29f75785fe51b55213a335458a77a038cbf870b5
satriadhapa <satriadhapa10@gmail.com>
2025-01-13 08:38:23 +0700

    fetch : create variabel with function

---
+++ day2.py
@@ -0,0 +1,15 @@
+x = "awesome"
+
+def fungsi():
+    print("python is " + x) 
+fungsi()
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
